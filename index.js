375452b7d2669f732279ac2b1240dd5ab3da6b33
Piyavadee Pariyavuth <piyavadeepariyavuth@gmail.com>
2018-01-02 01:47:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ function whisper(string) {
   return string.toLowerCase()
 }
 function logShout(string) {
-  console.log("spy was never called with `string.toUpperCase`")
+  console.log(string.toUpperCase)
 }
 function logWhisper(string) {
   console.log(string.toLowerCase)
