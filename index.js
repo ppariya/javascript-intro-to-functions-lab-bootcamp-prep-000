e0c749887b7da03c3773b5c319e88ee175a65727
Piyavadee Pariyavuth <piyavadeepariyavuth@gmail.com>
2018-01-02 01:44:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,9 +4,9 @@ function shout(string) {
 function whisper(string) {
   return string.toLowerCase()
 }
-function shout(string) {
+function logShout(string) {
   console.log(string.toUpperCase)
 }
-function whisper(string) {
+function logWhisper(string) {
   console.log(string.toLowerCase)
 }
